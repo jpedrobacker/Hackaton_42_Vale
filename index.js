62046f574a173ca145dff1ffb9dc213d743f07cb
jpedrobacker <jpedrobacker@outlook.com>
2024-12-07 15:48:08 -0300

    login page done

---
+++ index.js
@@ -0,0 +1,12 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function checkLogin()
+{
+	if (username.value === "carlos" && password.value === "1234")
+		window.location.href = "dashboard_carlos.html";
+	else if (username.value === "luisa" && password.value === "1234")
+		window.location.href = "dashboard_luisa.html";
+	else
+		document.getElementById("error-message").style.color = 'red';
+}
