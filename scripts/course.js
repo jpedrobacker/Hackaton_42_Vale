e14d62cd0c19f3a1c41079f7e089c9da1a4e271a
PradoAllan <allanprado.it@gmail.com>
2024-12-08 21:55:29 -0300

    add: quiz questions modal DONE

--- scripts/course.js
+++ scripts/course.js
@@ -2,7 +2,6 @@ const item1 = document.querySelectorAll('.main__course-content1');
 const item2 = document.querySelectorAll('.main__course-content2');
 const item3 = document.querySelectorAll('.main__course-content3');
 
-
 function openCoursePage1() {
 	console.log('openCoursePage1');
 	item1.forEach(item => {
@@ -41,3 +40,70 @@ function openCoursePage3() {
 		});
 	});
 }
+
+document.getElementById("submit-button").addEventListener("click", function () {
+	const selectedOption = document.querySelector('input[name="question0"]:checked');
+	const modal = document.getElementById("modal");
+	const modalMessage = document.getElementById("modal-message");
+	const closeButton = document.querySelector(".close-button");
+  
+	if (!selectedOption) {
+	  alert("Por favor, selecione uma opção.");
+	  return;
+	}
+  
+	if (selectedOption.value === "A" || selectedOption.value === "B" || selectedOption.value === "C") {
+	  modalMessage.textContent = `Justificativa: A opção A é a correta, 
+	  pois sempre que houver dúvida sobre a autenticidade de um e-mail, 
+	  especialmente um que solicite ações sensíveis como redefinição de senha, 
+	  o colaborador deve validar a informação diretamente com o setor responsável. 
+	  Isso evita que credenciais sejam roubadas.`;
+	  modal.classList.add("show");
+	  document.body.classList.add("modal-active");
+	}
+  
+	closeButton.addEventListener("click", function () {
+	  modal.classList.remove("show");
+	  document.body.classList.remove("modal-active");
+	});
+  
+	window.addEventListener("click", function (event) {
+	  if (event.target === modal) {
+		modal.classList.remove("show");
+		document.body.classList.remove("modal-active");
+	  }
+	});
+  });
+  
+  document.getElementById("submit-button2").addEventListener("click", function () {
+	const selectedOption = document.querySelector('input[name="questionCID"]:checked');
+	const modal = document.getElementById("modalCID");
+	const modalMessage = document.getElementById("modalCID-message");
+	const closeButton = document.querySelector(".closeCID-button");
+  
+	if (!selectedOption) {
+	  alert("Por favor, selecione uma opção.");
+	  return;
+	}
+  
+	if (selectedOption.value === "A" || selectedOption.value === "B" || selectedOption.value === "C") {
+	  modalMessage.textContent = `Justificativa: A opção A é a correta, pois, 
+	  sempre que houver dúvida sobre uma solicitação urgente, é essencial confirmar 
+	  a autenticidade da informação com fontes confiáveis. Luiz poderia, por exemplo, 
+	  ligar diretamente para o gerente de logística`;
+	  modal.classList.add("show");
+	  document.body.classList.add("modal-active");
+	}
+  
+	closeButton.addEventListener("click", function () {
+	  modal.classList.remove("show");
+	  document.body.classList.remove("modal-active");
+	});
+  
+	window.addEventListener("click", function (event) {
+	  if (event.target === modal) {
+		modal.classList.remove("show");
+		document.body.classList.remove("modal-active");
+	  }
+	});
+  });
